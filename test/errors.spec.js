4f633d82aea0ade3061dc3215284fb1ef78c7e26
Ruben Bridgewater <ruben@bridgewater.de>
2017-05-26 07:38:45 +0200

    chore: improve tests and remove obsolete dev dependency

--- test/errors.spec.js
+++ test/errors.spec.js
@@ -3,22 +3,44 @@
 /* eslint-env mocha */
 
 const assert = require('assert')
-const bufferFrom = require('buffer-from')
+const Buffer = require('buffer').Buffer
 const errors = require('../')
 const ReplyError = errors.ReplyError
 const ParserError = errors.ParserError
 const RedisError = errors.RedisError
+const AbortError = errors.AbortError
+const InterruptError = errors.InterruptError
 
 describe('errors', function () {
+  const redisError = new RedisError('test')
+  const replyError = new ReplyError('test')
+  const parserError = new ParserError('test', Buffer.from('\r\nt+est\r\n'), 3)
+  const abortError = new AbortError('test')
+  const interruptError = new InterruptError('test')
+
   it('errors should have a stack trace with error message', function () {
-    const err1 = new RedisError('test')
-    const err2 = new ReplyError('test')
-    const err3 = new ParserError('test', bufferFrom(''), 0)
-    assert(err1.stack)
-    assert(err2.stack)
-    assert(err3.stack)
-    assert(/RedisError: test/.test(err1.stack))
-    assert(/ReplyError: test/.test(err2.stack))
-    assert(/ParserError: test/.test(err3.stack))
+    assert(redisError.stack)
+    assert(replyError.stack)
+    assert(parserError.stack)
+    assert(abortError.stack)
+    assert(interruptError.stack)
+    assert(/RedisError: test/.test(redisError.stack))
+    assert(/ReplyError: test/.test(replyError.stack))
+    assert(/ParserError: test/.test(parserError.stack))
+    assert(/AbortError: test/.test(abortError.stack))
+    assert(/InterruptError: test/.test(interruptError.stack))
+  })
+
+  it('should properly inherit from each other', function () {
+    assert(redisError instanceof Error)
+    assert(replyError instanceof RedisError)
+    assert(parserError instanceof RedisError)
+    assert(abortError instanceof RedisError)
+    assert(interruptError instanceof AbortError)
+  })
+
+  it('parser errors should contain properties', function () {
+    assert(parserError.offset)
+    assert(parserError.buffer)
   })
 })
